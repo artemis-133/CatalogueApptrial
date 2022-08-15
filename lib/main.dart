79c1af6fd428003b4aa0f61a80dd1489593ecc21
artemis-133 <2019UEE0133@iitjammu.ac.in>
2022-08-15 15:50:34 +0530

    Day-4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:project_1/pages/home_page.dart';
 import 'package:project_1/pages/login.dart';
 import 'pages/home_page.dart';
@@ -13,16 +14,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        // fontFamily: GoogleFonts.lato().fontFamily,
+        textTheme: GoogleFonts.latoTextTheme(textTheme),
+      ),
       darkTheme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: Brightness.light,
       ),
       routes: {
-        "/" :(context) => Login(),
-        "/login":(context) => Login(),
-        "/homepage":(context) => Homepage(),
+        "/": (context) => Login(),
+        "/login": (context) => Login(),
+        "/homepage": (context) => Homepage(),
       },
     );
   }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,15 +5,61 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-            style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_page.png",
+            fit: BoxFit.cover,
           ),
-        ),
+          SizedBox(
+            height: 20.0,
+            //child: Text("haelkdad"),
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+            //child: Text("haelkdad"),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  //child: Text("haelkdad"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Succesful");
+                  },
+                  style: TextButton.styleFrom(),
+                  child: Text("Login"),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
